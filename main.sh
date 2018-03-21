d9f144a5c82e5b539aa8f9de897553804b8f44e3
Chris Hanaoka <chanaoka@polyverse.io>
2018-03-21 15:53:11 -0700

    added basic error-checking.

--- main.sh
+++ main.sh
@@ -2,12 +2,26 @@
 
 which wget 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
-	echo "Please install wget."
+	echo "Error: please install wget."
 	exit 1
 fi
 
 if [ $# -eq 0 ]; then
-	SCRIPT_LOCATION=`mktemp`
-	wget -qO $SCRIPT_LOCATION https://raw.githubusercontent.com/polyverse/plv/master/scripts/help
-	sh $SCRIPT_LOCATION
+	CMD="help"
+else
+	CMD="$1"
 fi
+
+_exit() {
+	rm $SCRIPT_LOCATION 2>/dev/null || true
+	exit $1
+}
+
+SCRIPT_LOCATION=`mktemp`
+wget -qO $SCRIPT_LOCATION https://raw.githubusercontent.com/polyverse/plv/master/scripts/$CMD
+if [ $? -ne 0 ]; then
+	echo "Error: unknown subcommand '$CMD'."
+	_exit
+fi
+sh $SCRIPT_LOCATION
+_exit $?
