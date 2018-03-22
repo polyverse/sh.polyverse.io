8c793347f81d62a5f734bfa2dd135620cf30f80d
Chris Hanaoka <chanaoka@polyverse.io>
2018-03-21 21:16:34 -0700

    tweaks based on CentOS testing.

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-which wget 2>&1 > /dev/null
+wget -V >/dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "Error: please install wget."
 	exit 1
@@ -8,19 +8,19 @@ fi
 
 if [ $# -eq 0 ] || [ "$1" = "help" ]; then
 	wget -qO- --no-cache https://raw.githubusercontent.com/polyverse/plv/master/usage.txt
-	exit
+	exit 1
 fi
 
 _exit() {
-	rm $SCRIPT_LOCATION 2>/dev/null || true
+	rm $SCRIPT 2>/dev/null || true
 	exit $1
 }
 
-SCRIPT_LOCATION=`mktemp`
-wget -qO $SCRIPT_LOCATION --no-cache https://raw.githubusercontent.com/polyverse/plv/master/scripts/$1
+SCRIPT=`mktemp -t plv.XXX`
+wget -qO $SCRIPT --no-cache https://raw.githubusercontent.com/polyverse/plv/master/scripts/$1.sh
 if [ $? -ne 0 ]; then
 	echo "Error: unknown subcommand '$1'."
 	_exit
 fi
-sh $SCRIPT_LOCATION
+sh $SCRIPT
 _exit $?
