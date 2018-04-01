b1a9108f4969388659cb62b60b924a94563a817a
Chris Hanaoka <chanaoka@polyverse.io>
2018-03-31 23:36:28 -0700

    fixed bug where didn't work if no subdirectories; changed output format to be more uniform and include time/date.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -3,28 +3,30 @@
 INSTALL_ROOT=$PWD
 
 echo "Scanning $INSTALL_ROOT..."
-find $INSTALL_ROOT -name \* -print | while read line; do
+find $INSTALL_ROOT/ -name \* -print | while read line; do
         TARGET="$(echo $line | sed 's|'$INSTALL_ROOT'/||g')"
         if [ "$INSTALL_ROOT" = "$TARGET" ]; then
                 continue
         fi
 
-	STAT="$(stat --format "%A" "$INSTALL_ROOT/$TARGET")"
+        STAT="$(stat --format "%A" "$INSTALL_ROOT/$TARGET")"
 
         if [ "$(echo "$STAT" | grep "^-.*x")" = "" ]; then
                 continue
         fi
 
-	ELF_COMMENTS="$(readelf --string-dump=.comment $INSTALL_ROOT/$TARGET 2>/dev/null)"
-	if [ $? -ne 0 ]; then
-		IS_PV="false"
-	else
-		if [ "$(echo "$ELF_COMMENTS" | grep "\-PV\-")" = "" ]; then
-			IS_PV="false"
-		else
-			IS_PV="true"
-		fi
-	fi
+        DTSTAMP="$(stat --format "%y" "$INSTALL_ROOT/$TARGET")"
 
-	printf "[%s] %s %s\n" $IS_PV $STAT $line
+        ELF_COMMENTS="$(readelf --string-dump=.comment $INSTALL_ROOT/$TARGET 2>/dev/null)"
+        if [ $? -ne 0 ]; then
+                IS_PV="-not elf-"
+        else
+                if [ "$(echo "$ELF_COMMENTS" | grep "\-PV\-")" = "" ]; then
+                        IS_PV="-vanilla-"
+                else
+                        IS_PV="scrambled"
+                fi
+        fi
+
+        printf "[%s] %s %s %s\n" "$IS_PV" "$STAT" "$DTSTAMP" "$line"
 done
