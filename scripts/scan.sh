21114c2b9eefb3642a0a2c6ac13d9cb31088cec2
Chris Hanaoka <chanaoka@polyverse.io>
2018-03-31 17:40:19 -0700

    scans recursively from current directory and tells you if executables are scrambled.

---
+++ scripts/scan.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+INSTALL_ROOT=$PWD
+
+echo "Scanning $INSTALL_ROOT..."
+find $INSTALL_ROOT -name \* -print | while read line; do
+        TARGET="$(echo $line | sed 's|'$INSTALL_ROOT'/||g')"
+        if [ "$INSTALL_ROOT" = "$TARGET" ]; then
+                continue
+        fi
+
+	STAT="$(stat --format "%A" "$INSTALL_ROOT/$TARGET")"
+
+        if [ "$(echo "$STAT" | grep "^-.*x")" = "" ]; then
+                continue
+        fi
+
+	ELF_COMMENTS="$(readelf --string-dump=.comment $INSTALL_ROOT/$TARGET 2>/dev/null)"
+	if [ $? -ne 0 ]; then
+		IS_PV="false"
+	else
+		if [ "$(echo "$ELF_COMMENTS" | grep "\-PV\-")" = "" ]; then
+			IS_PV="false"
+		else
+			IS_PV="true"
+		fi
+	fi
+
+	printf "[%s] %s %s\n" $IS_PV $STAT $line
+done
