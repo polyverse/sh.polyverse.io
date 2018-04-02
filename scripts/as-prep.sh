7c08d1ca2059c298a7c484e1fd1bd265aa35f82e
Chris Hanaoka <chanaoka@polyverse.io>
2018-04-02 15:31:02 -0700

    new script to prep vm's.

---
+++ scripts/as-prep.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+GATEWAY_IP="52.42.103.75"
+
+usage() {
+cat >&2 <<-EOF
+
+Usage:
+
+  plv as-prep <authkey> <node_id> <node_role>
+
+Node Roles: esm611pv | arcmc27pv | logger65pv | sconnhostpv
+
+EOF
+}
+
+if [ $# -ne 3 ]; then
+	usage
+	exit 1
+fi
+
+AUTHKEY="$1"
+NODEID="$2"
+NODEROLE="$3"
+
+if [ ! -d /opt.old ]; then
+	echo "Backup: creating a copy of /opt to /opt.old..."
+	cp -R /opt /opt.old
+else
+	echo "Backup: /opt.old already exists. Skipping."
+fi
+
+echo "Creating /opt/polyverse/nodeid..."
+mkdir -p /opt/polyverse
+echo $NODEID > /opt/polyverse/nodeid
+
+case $NODEROLE in
+	esm611pv)
+		echo "Run '/etc/init.d/arcsight_services stop'."
+		;;
+	arcmc27pv)
+		echo "Run '/etc/rc.d/init.d/arcsight_arcmc stop'."
+		;;
+	logger65pv)
+		echo "Run '/etc/rc.d/init.d/arcsight_logger stop'."
+		;;
+	sconnhostpv)
+		echo "No need to stop any services."
+		;;
+	docker)
+		echo "This is a test environment. No services to start/stop. Skipping."
+	*)
+		echo "Error: unknown node_role '$NODEROLE'. Exiting..."
+		exit 1
+esac
+echo $NODEROLE > /opt/polyverse/noderole
+
+if [ "$(cat /etc/hosts | grep "repo.polyverse.io")" = "" ]; then
+	echo "Dedicated BigBang stack: adding /etc/hosts entry..."
+	echo "$GATEWAY_IP repo.polyverse.io" >> /etc/hosts
+else
+	echo "Dedicated BigBang stack: /etc/hosts entry already exists. Skipping."
+fi
+
+if [ ! -f /etc/yum.repos.d/polyverse.repo ]; then
+	echo "Polymorphic Linux: installing..."
+	curl https://repo.polyverse.io/install.sh | sh -s $AUTHKEY $NODEID
+	if [ $? -ne 0 ]; then
+		echo "Encountered error. Exiting..."
+		exit 1
+	fi
+else
+	echo "Polymorphic Linux: already installed. Skipping."
+fi
