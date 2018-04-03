e6b2d6d23b5509d9d16a41734320f54c5ccb8cba
rsundahl <roy@polyverse.io>
2018-04-03 14:05:26 -0700

    Added .gitignore and scripts/scan-pid.sh.

---
+++ scripts/scan-pid.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [[ $# != 2 ]] ; then
+	echo 'usage: sudo $0 pid string'
+	echo 'example: sudo $0 `pgrep java` "\-PV\-"'
+	exit 1
+fi
+
+pid=${1}
+string=${2}
+
+mkdir -p "${pid}"
+cd "${pid}"
+
+# Get a copy of the process maps
+cp /proc/${pid}/maps .
+
+# Fix for loop using whole line
+oIFS=${IFS}
+IFS='
+'
+
+# Go through each range of memory and look for the string using gdb
+for line in $(cat maps); do
+	start=$(echo -n ${line} | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | cut -d" " -f1)
+	stop=$(echo -n ${line} | sed -n 's/^\([0-9a-f]*\)-\([0-9a-f]*\) .*$/\1 \2/p' | cut -d" " -f2)
+
+	filename="${pid}-${start}-${stop}.dump"
+	gdb --quiet --batch-silent -pid ${pid} -ex "dump memory ${filename} 0x${start} 0x${stop}" 2>/dev/null
+
+	if [ ! -e "$filename" ]; then
+		echo "[-MISSING-] ${line}"
+	elif result=$(grep ${string} ${filename}); then
+		echo "[POLYVERSE] ${line}"
+	else
+		echo "[-VANILLA-] ${line}"
+		rm ${filename}
+	fi
+done
+
+cd ..
+IFS=${oIFS}
+
