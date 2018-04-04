d5cee791bf422acfc583417e813e93d3baa3333a
Chris Hanaoka <chanaoka@polyverse.io>
2018-04-04 14:16:36 -0700

    added ... arguments to scan multiple folders.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,40 +1,41 @@
 #!/bin/sh
-
+  
 if [ "$1" = "" ]; then
-	INSTALL_ROOT=$PWD
+        SCAN_PATHS="$PWD"
 else
-	INSTALL_ROOT=$1
+        SCAN_PATHS="$@"
 fi
 
-echo "Scanning $INSTALL_ROOT..."
-find $INSTALL_ROOT/ -name \* -print | while read line; do
-        TARGET="$(echo $line | sed 's|'$INSTALL_ROOT'/||g')"
-        if [ "$INSTALL_ROOT" = "$TARGET" ]; then
-                continue
-        fi
-
-        STAT="$(stat --format "%A" "$INSTALL_ROOT/$TARGET")"
-
-        if [ "$(echo "$STAT" | grep "^-.*x")" = "" ]; then
-                continue
-        fi
-
-        DTSTAMP="$(stat --format "%y" "$INSTALL_ROOT/$TARGET" | awk '{print $1 " " $2}' | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')"
-
-	CHECKSUM="$(cksum "$INSTALL_ROOT/$TARGET" | awk '{print $1}')"
-
-        ELF_COMMENTS="$(readelf --string-dump=.comment $INSTALL_ROOT/$TARGET 2>/dev/null)"
-        if [ $? -ne 0 ]; then
-                IS_PV="-not elf--"
-        else
-                if [ "$(echo "$ELF_COMMENTS" | grep "\-PV\-")" = "" ]; then
-                        IS_PV="-vanilla--"
-                else
-                        IS_PV="scrambled"
-			SHA="$(echo "$ELF_COMMENTS" | awk -F'(' '{print $2}' | awk -F')' '{print $1}' | awk -F'-' '{print $3}' | xargs)"
-			IS_PV="PV-$SHA"
+for SCAN_PATH in $SCAN_PATHS; do
+        echo "Scanning $SCAN_PATH..."
+        find "$SCAN_PATH" -name \* -print | while read line; do
+                TARGET="$(echo $line | sed 's|'$SCAN_PATH'/||g')"
+                if [ "$SCAN_PATH" = "$TARGET" ]; then
+                        continue
                 fi
-        fi
 
-        printf "[%s] %s %-11s %s  %s\n" "$IS_PV" "$STAT" "$CHECKSUM" "$DTSTAMP" "$line"
+                STAT="$(stat --format "%A" "$SCAN_PATH/$TARGET")"
+
+                if [ "$(echo "$STAT" | grep "^-.*x")" = "" ]; then
+                        continue
+                fi
+
+                DTSTAMP="$(stat --format "%y" "$SCAN_PATH/$TARGET" | awk '{print $1 " " $2}' | sed -E 's/(:[0-9]+)\.[0-9]+/\1/g')"
+
+                CHECKSUM="$(cksum "$SCAN_PATH/$TARGET" | awk '{print $1}')"
+
+                ELF_COMMENTS="$(readelf --string-dump=.comment $SCAN_PATH/$TARGET 2>/dev/null)"
+                if [ $? -ne 0 ]; then
+                        IS_PV="-not elf--"
+                else
+                        if [ "$(echo "$ELF_COMMENTS" | grep "\-PV\-")" = "" ]; then
+                                IS_PV="-vanilla--"
+                        else
+                                IS_PV="scrambled"
+                                SHA="$(echo "$ELF_COMMENTS" | awk -F'(' '{print $2}' | awk -F')' '{print $1}' | awk -F'-' '{print $3}' | xargs)" 
+                                IS_PV="PV-$SHA"
+                        fi      
+                fi
+                printf "[%s] %s %-11s %s  %s\n" "$IS_PV" "$STAT" "$CHECKSUM" "$DTSTAMP" "$line"
+        done
 done
