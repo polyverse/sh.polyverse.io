2feb588a0275ee9e4b3080e16aaae06e5b05c7bc
Chris Hanaoka <chanaoka@polyverse.io>
2018-04-26 15:38:04 -0700

    refactored url construction; added fedora support.

--- scripts/get-package-url.sh
+++ scripts/get-package-url.sh
@@ -1,65 +1,118 @@
 #!/bin/sh
-  
-LOCS=""
-LOCS="$LOCS http://vault.centos.org/6.0/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.0/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.1/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.1/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.2/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.2/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.3/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.3/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.4/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.4/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.5/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.5/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.6/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.6/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.7/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.7/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.8/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.8/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.9/os/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/6.9/updates/x86_64/Packages/"
-LOCS="$LOCS http://vault.centos.org/7.0.1406/os/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.0.1406/updates/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.1.1503/os/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.1.1503/updates/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.2.1511/os/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.2.1511/updates/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.3.1611/os/x86_64/Packages"
-LOCS="$LOCS http://vault.centos.org/7.3.1611/updates/x86_64/Packages"
-LOCS="$LOCS http://mirror.centos.org/centos/7.4.1708/os/x86_64/Packages/"
-LOCS="$LOCS http://mirror.centos.org/centos/7.4.1708/updates/x86_64/Packages/"
-LOGS="$LOCS http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/23/Everything/x86_64/os/"
-LOGS="$LOCS http://download.fedoraproject.org/pub/archive/fedora/linux/updates/23/x86_64/"
-LOGS="$LOCS http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/24/Everything/x86_64/os/"
-LOGS="$LOCS http://download.fedoraproject.org/pub/archive/fedora/linux/updates/24/x86_64/"
-LOGS="$LOCS http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/25/Everything/x86_64/os/"
-LOGS="$LOCS http://download.fedoraproject.org/pub/archive/fedora/linux/updates/25/x86_64/"
+
+function apk_alpine() {
+        (>&2 echo "error: support for alpine not yet implemented.")
+        return 1
+}
+
+function deb_ubuntu() {
+	(>&2 echo "error: support for ubuntu not yet implemented.")
+	return 1
+}
+
+function rpm_fedora() {
+	local FILENAME="$1"
+	local RELEASE="$(echo $FILENAME | sed 's/.*fc\([0-9][0-9]\).*/\1/')"
+	local FIRST_LETTER=${FILENAME:0:1}
+
+	if [ -z "$RELEASE" ]; then
+		(>&2 echo "error: unable to determine fedora release for '$FILENAME'.")
+		return 1
+	fi
+
+	URLs=""
+	URLs="$URLs http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/${RELEASE}/Everything/x86_64/os/Packages/${FIRST_LETTER}"
+	URLs="$URLs http://download.fedoraproject.org/pub/archive/fedora/linux/updates/${RELEASE}/x86_64/Packages/${FIRST_LETTER}"
+
+	return 0
+}
+
+function rpm_centos() {
+	local RELEASES="6.0 6.1 6.2 6.3 6.4 6.5 6.6 6.7 6.8 6.9 7.0.1406 7.1.1503 7.2.1511 7.3.1611"
+
+	URLs=""
+	for RELEASE in $RELEASES; do
+		URLs="$URLs http://vault.centos.org/${RELEASE}/os/x86_64/Packages"
+		URLs="$URLs http://vault.centos.org/${RELEASE}/updates/x86_64/Packages"
+	done
+
+	URLs="$URLs http://mirror.centos.org/centos/7/os/x86_64/Packages"
+	URLs="$URLs http://mirror.centos.org/centos/7/updates/x86_64/Packages"
+
+	return 0
+}
+
+function get_format() {
+	local FILENAME="$1"
+	local FORMAT="${FILENAME##*.}"
+
+	if [ "$FORMAT" != "rpm" ] && [ "$FORMAT" != "apk" ] && [ "$FORMAT" != "deb" ]; then
+		(>&2 echo "error: unsupported type '$FORMAT'")
+		return 1
+	fi
+
+	echo "$FORMAT"
+	return 0
+}
+
+function get_project() {
+	local FILENAME="$1"
+	local FORMAT="$(get_format $FILENAME)"
+	local PROJECT=""
+
+	case $FORMAT in
+		apk)
+			PROJECT="alpine"
+			;;
+		deb)
+			PROJECT="ubuntu"
+			;;
+		rpm)
+			if [ ! -z "$(echo "$FILENAME" | tr "." "\n" | grep -e "fc[0-9][0-9]")" ]; then
+				PROJECT="fedora"
+			else
+				PROJECT="centos"
+			fi
+			;;
+		*)
+			(>&2 echo "error: unable to determine project for type '$FORMAT'")
+			return 1
+	esac
+	
+	echo "$PROJECT"
+	return 0		
+}
 
 function GetPackageURL() {
-        FILENAME="$1"
+        local FILENAME="$1"
 
-        for LOC in $LOCS; do
-                (>&2 printf ".")
-                if [ ! -z "$(curl -sI $LOC/$FILENAME | grep "200 OK")" ]; then
-                        (>&2 printf "\n")
-                        echo $LOC/$FILENAME
+        for URL in $URLs; do
+		(>&2 echo "Checking $URL...")
+                if [ ! -z "$(curl -sI $URL/$FILENAME | grep "200 OK")" ]; then
+                        echo "$URL/$FILENAME"
                         return 0
                 fi
         done
 
-        (>&2 printf "\n")
         return 1
 }
 
-RESULT="$(GetPackageURL $1)"
-EXITCODE="$?"
+FORMAT="$(get_format $1)"
+if [ $? -ne 0 ]; then exit 1; fi
 
-if [ $EXITCODE -ne 0 ]; then
-        (>&2 echo "Not found.")
-        exit 1
+PROJECT="$(get_project $1)"
+if [ $? -ne 0 ]; then exit 1; fi
+
+# call function called <format>_<project> to populate LOCS variable
+${FORMAT}_${PROJECT} $1
+if [ $? -ne 0 ]; then exit 1; fi
+
+RESULT="$(GetPackageURL $1)"
+if [ $? -ne 0 ]; then
+	(>&2 echo "Not found.")
+	exit 1
 fi
 
+# write the final result to stdout
 echo "$RESULT"
+exit 0
