318433cc36478ea03aebc509313d975d9801185b
Chris Hanaoka <chanaoka@polyverse.io>
2019-12-05 13:36:25 -0800

    /cli added to base url.

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -z "$PV_BASE_URL" ]; then PV_BASE_URL="https://repo.polyverse.io"; fi
+if [ -z "$PV_BASE_URL" ]; then PV_BASE_URL="https://repo.polyverse.io/cli"; fi
 
 PV_SHELL="sh"
 if [ ! -z "$(echo $SHELL | grep bash)" ]; then
@@ -26,11 +26,11 @@ if [ -z "$SUBCMD" ]; then
 	SUBCMD="cli"
 fi
 
-curl -s --fail $PV_BASE_URL/cli/$SUBCMD >/dev/null 2>&1
+curl -s --fail $PV_BASE_URL/$SUBCMD >/dev/null 2>&1
 if [ $? -ne 0 ]; then
 	echo "error: unknown subcommand '$SUBCMD'."
 	exit 1
 fi
 
-eval "curl -sS $PV_BASE_URL/cli/$SUBCMD | $PV_SHELL -s $ARGS"
+eval "curl -sS $PV_BASE_URL/$SUBCMD | $PV_SHELL -s $ARGS"
 exit $?
